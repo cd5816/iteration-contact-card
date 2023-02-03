4a275a85d98ffe37b70110189a61e4e613cd222a
Caleb Dismuke <109627597+cd5816@users.noreply.github.com>
2023-02-03 14:00:48 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import ContactCard from "./ContactCard";
+
+const data = [
+  {
+    id: "sunita-abc123",
+    name: "Kelly Dismuke",
+    job: "CEO",
+    email: "sunita.kumar@acme.co"
+  },
+  {
+    id: "henderson-def456",
+    name: "Ella",
+    job: "Receptionist",
+    email: "henderson-the-second@acme.co"
+  },
+  {
+    id: "aio-ghi789",
+    name: "Caleb Dismuke",
+    job: "Hacker",
+    email: "kobayashi.aoi@acme.co"
+  }
+];
+
+function App() {
+  return (
+    <ul>
+      {data.map(({ id, name, job, email }) => (
+        <ContactCard
+          key={id} // Every item needs a unique identifier
+          name={name}
+          job={job}
+          email={email}
+        />
+      ))}
+    </ul>
+  );
+}
+
+// The longer way
+// const element = data.map((contact) => (
+//   <ContactCard
+//     key={contact.id} // Every item needs a unique identifier
+//     name={contact.name}
+//     job={contact.job}
+//     email={contact.email}
+//   />
+// ));
+// return <ul>{element}</ul>;
+// }
+
+export default App;
